648eef6edc58a7472b5b774e6725c9c1a76a1bbc
Teng Lin <tenglin2@gmail.com>
2018-08-25 11:18:34 -0400

    Clear former code to test boxes.

--- jst.js
+++
@@ -1,4 +0,0 @@
-const innerBox = document.createElement("div");
-innerBox.setAttribute("style", "height: 200px; width: 200px; background-color: pink; margin-left: auto; margin-right: auto;");
-const outerBox = document.querySelector(".box");
-outerBox.appendChild(innerBox);
