ea9786d8bfdbd271191eda69e5b5d143827a02f9
Teng Lin <tenglin2@gmail.com>
2018-08-25 19:28:30 -0400

    Testing to see how textContent can be manipulated.

---
+++ jst.js
@@ -0,0 +1,8 @@
+var stringOutput = document.getElementById("stringOutput");
+stringOutput.textContent = "Chump";
+var mainOutput = document.getElementById("mainOutput");
+stringOutput.textContent += "shushamushamsosconstantinople";
+console.log(stringOutput.textContent);
+console.log(mainOutput.textContent);
+const mainy = document.querySelector("#mainScreen p");
+mainy.textContent = "Superduper";
