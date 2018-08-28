b4f2e34b6f6a8cc7fbe0f00a4cbb3fdb404bc0e0
Teng Lin <tenglin2@gmail.com>
2018-08-28 18:36:18 -0400

    Deleted buildup comments. Plan to add clear explanation comments after I do keyboard and touch support.

--- jst.js
+++ jst.js
@@ -1,29 +1,4 @@
-// var stringOutput = document.getElementById("stringOutput");
-// stringOutput.textContent = "Chump";
-// var mainOutput = document.getElementById("mainOutput");
-// stringOutput.textContent += "shushamushamsosconstantinople";
-// console.log(stringOutput.textContent);
-// console.log(mainOutput.textContent);
-//
-// const mainScreen = document.getElementById("mainScreen");
-// mainScreen.children[0].textContent = "Hello boys";
-// console.log(mainScreen.innerHTML);
-
-// Okay, so far I have a good idea of what I have to do. Each button has a different process associated with it. Clear, delete, help is pretty independent. The idea is to have a main output that takes in numbers specifically and when a operator button is clicked, we push it all to the string output. The event of equal sign being clicked will call a function that evaluates the string output, which will probably be the most difficult part since we have to do order of operations. Maybe a multi step process with validation first and foremost. Parenthesis priority will take a while though it really is just slicing the portion, evaluating and returning it back kinda like recursion. +- sign change is simply to change the sign of the mainOutput to negative. Don't forget to convert string to number when evaluating.
-
-// var stringOutput = document.getElementById("stringOutput");
-// const buttons = document.querySelectorAll(".button");
-// buttons.forEach(button => {
-//   button.addEventListener("click", (e) => {
-//     stringOutput.textContent += button.children[0].textContent;
-//   })
-// })
-
-// isNaN(passiveExpression.textContent.slice(-1)) && passiveExpression.textContent.slice(-1) != "." && passiveExpression.textContent.slice(-1) != "(" &&
-// passiveExpression.textContent.slice(-1) != ")"
-
-
-// Okay our next part we have to create the evaluate function. No we don't. All we need to do is handle the sign issues. I think I can just use the eval function though I have to handle the sign changes.
+// Plan on adding support for keyboard and touchscreen by simply adding eventListeners for keydown as well as touch. Right now the calculator is functional through mouse clicks so that's pretty much the assignment, but I want to make it something I am proud of and can show off. So more work later.
 
 var activeExpression = document.getElementById("activeExpression");
 var passiveExpression = document.getElementById("passiveExpression");
@@ -41,16 +16,6 @@ function activeInput(activeButtons){
         }else if(activeExpression.textContent.slice("-1") === ")"){
           console.log("Error, do nothing.");
         }
-        // else if(passiveExpression.textContent.slice(-1) === "=" && !(isNaN(button.children[0].textContent))){
-        //   console.log("1 happens");
-        //   passiveExpression.textContent = "";
-        //   activeExpression.textContent = "";
-        // }else if(passiveExpression.textContent.slice(-1) === "=" && isNaN(button.children[0].textContent)){
-        //   console.log("2 happens");
-        //   passiveExpression.textContent = activeExpression.textContent + button.children[0].textContent;
-        //   activeExpression.textContent = "";
-        //   return;
-        // }
         activeExpression.textContent += button.children[0].textContent;
       }
 
@@ -87,9 +52,6 @@ function passiveInput(passiveButtons){
           passiveExpression.textContent += button.children[0].textContent;
           activeExpression.textContent = "";
         }
-
-        // passiveExpression.textContent += activeExpression.textContent + button.children[0].textContent;
-        // activeExpression.textContent = "";
       }
 
 
@@ -118,23 +80,6 @@ function evaluate(){
     passiveExpression.textContent += "=";
   }
 }
-
-// const activeButtons = document.querySelectorAll(".active");
-// activeButtons.forEach(button => {
-//   button.addEventListener("click", (e) => {
-//     activeExpression.textContent += button.children[0].textContent;
-//   })
-// });
-
-// console.log("Hello");
-// const active = document.querySelectorAll(".active");
-// active.forEach(activeButton => {
-//   activeButton.addEventListener("click", (e) => {
-//     console.log("Hello");
-//     // activeExpression.textContent += "crest";
-//   })
-// });
-
 
 
 
